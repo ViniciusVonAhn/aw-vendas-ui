207aa0dc38bfa0ac1743fae626d926cb550308f5
Vinicius Von Ahn <viniciusvonahn@gmail.com>
2018-10-20 23:02:00 -0300

    a

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/aw-vendas-ui'));
+app.use(express.static('/dist/aw-vendas-ui'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/aw-vendas-ui/index.html'));
+  res.sendFile(path.join('/dist/aw-vendas-ui/index.html'));
 });
 app.listen(process.env.PORT || 8080);
